2843de504afe235cbdff5aeb52cc882ca14c8049
laon2019 <laon5987@nate.com>
2023-06-14 22:29:10 +0900

    0614클릭감지

--- 09/raycaster/src/ex01.js
+++ 09/raycaster/src/ex01.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // ----- 주제: 
 
@@ -22,6 +23,7 @@ export default function example() {
 		0.1,
 		1000
 	);
+	camera.position.x = 5;
 	camera.position.y = 1.5;
 	camera.position.z = 4;
 	scene.add(camera);
@@ -36,20 +38,56 @@ export default function example() {
 	scene.add(directionalLight);
 
 	// Controls
+	const controls = new OrbitControls(camera, renderer.domElement);
 
 	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshStandardMaterial({
-		color: 'seagreen'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+	const lineMaterial = new THREE.LineBasicMaterial({color:'yellow'});
+	const points = [];
+	points.push(new THREE.Vector3(0, 0, 100));
+	points.push(new THREE.Vector3(0, 0, -100));
+	const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+	const guide = new THREE.Line(lineGeometry, lineMaterial);
+	scene.add(guide);
 
+	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const boxMaterial = new THREE.MeshStandardMaterial( {color:'plum'});
+	const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+	boxMesh.name = "box";
+
+	const torusGeometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
+	const torusMaterial = new THREE.MeshStandardMaterial({ color:"lime" });
+	const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+	torusMesh.name = "torus";
+
+	scene.add(boxMesh, torusMesh);
+
+	const meshes = [boxMesh, torusMesh];
+
+	const raycaster = new THREE.Raycaster();
+	
 	// 그리기
 	const clock = new THREE.Clock();
 
 	function draw() {
-		const delta = clock.getDelta();
+		// const delta = clock.getDelta();
+		const time = clock.getElapsedTime();
+
+		boxMesh.position.y = Math.sin(time)*2;
+		torusMesh.position.y = Math.cos(time)*2;
+		boxMesh.material.color.set('plum');
+		torusMesh.material.color.set('lime');
+
+		const origin = new THREE.Vector3(0,0,100);
+		// const direction = new THREE.Vector3(0, 0, -1);
+		const direction = new THREE.Vector3(0, 0, -100);
+		direction.normalize(); // 정규화
+		raycaster.set(origin, direction);
+
+		const intersects = raycaster.intersectObjects(meshes);
+		intersects.forEach(item =>{
+			// console.log(item.object.name);
+			item.object.material.color.set('red');
+		})
 
 		renderer.render(scene, camera);
 		renderer.setAnimationLoop(draw);
